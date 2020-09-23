5ef19862126a90b53bc50ffbad58983c16af91a8
Lars van den Brandt <Lars.vandenbrandt@me.com>
2020-09-23 15:40:38 +0200

    hambuger menu

--- socialform_web_app/src/components/BodyIndex.js
+++ socialform_web_app/src/components/BodyIndex.js
@@ -6,7 +6,52 @@ import { CardDeck } from "react-bootstrap";
 
 function BodyIndex() {
   return (
-    <div class="row mt-5 ml-2 justify-content-left">
+    <div class="row mt-5 ml-2 justify-content-center">
+      <div class="card" style={{ width: "300px" }}>
+        <a href="Photodetails">
+          <img className="card-image-index" src={testPhoto} alt="Logo" />
+        </a>
+        <div class="card-body">
+          <a class="card-title-index" href="username">
+            @Gebruikersnaam
+          </a>
+          <p class="card-text-index">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </div>
+
+      <div class="card" style={{ width: "300px" }}>
+        <a href="Photodetails">
+          <img className="card-image-index" src={testPhoto} alt="Logo" />
+        </a>
+        <div class="card-body">
+          <a class="card-title-index" href="username">
+            @Gebruikersnaam
+          </a>
+          <p class="card-text-index">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </div>
+
+      <div class="card" style={{ width: "300px" }}>
+        <a href="Photodetails">
+          <img className="card-image-index" src={testPhoto} alt="Logo" />
+        </a>
+        <div class="card-body">
+          <a class="card-title-index" href="username">
+            @Gebruikersnaam
+          </a>
+          <p class="card-text-index">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </div>
+
       <div class="card" style={{ width: "300px" }}>
         <a href="Photodetails">
           <img className="card-image-index" src={testPhoto} alt="Logo" />
