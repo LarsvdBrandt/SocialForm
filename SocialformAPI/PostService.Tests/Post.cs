ab880f2666858532816770b2db6a86fb5bf68cfb
Lars van den Brandt <Lars.vandenbrandt@me.com>
2020-12-02 15:02:13 +0100

    start unit testing

---
+++ SocialformAPI/PostService.Tests/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PostService.Tests
+{
+    [TestClass]
+    public class Post
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
